d4df5f36e1333976d6c98e3722546b093b24262e
ernie casilla <ecasilla@icloud.com>
2015-08-18 23:44:32 -0400

    fixing some old bugs

--- test/unit/hello_spec.js
+++ test/unit/hello_spec.js
@@ -1,14 +1,1 @@
-var hello = require("../../app/scripts/hello"),
-	expect = require("../helpers/expect");
-
-describe('hello', function() {
-  it('should say hello', function() {
-      expect(hello()).to.be('hello world');
-  });
-  it('should say hello to person', function() {
-      expect(hello('Directv')).to.be('hello Directv');
-  });
-    it('should not say change your name', function() {
-      expect(hello('Dtv')).to.not.be('hello Directv');
-  });
-});
+var hello = require('../../app/scripts/hello.js');
